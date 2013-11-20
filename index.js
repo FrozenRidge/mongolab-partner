c93cba5b1efac096ed04bc9114a4e8b10e701fc3
Niall O'Higgins <niallo@frozenridge.co>
2013-11-19 17:10:41 -0800

    moar

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ var request = require('superagent')
 
 var API_URL = "https://api.mongolab.com/api/1/"
 
-module.exports = function(opts) {
+module.exports = function(config) {
   return {
+    viewPartner: function(cb) {
+      request
+        .get(API_URL + "partners/" + config.username)
+        .auth(config.username, config.password)
+        .end(function(res) {
+          if (res.error) {
+            return cb(res.error, res)
+          }
+          return cb(null, res)
+        })
+    },
+
     viewAccounts: function(cb) {
       request
-        .get(API_URL + "partners/" + opts.username + "/accounts")
-        .auth(opts.username, opts.password)
+        .get(API_URL + "partners/" + config.username + "/accounts")
+        .auth(config.username, config.password)
+        .end(function(res) {
+          if (res.error) {
+            return cb(res.error, res)
+          }
+          return cb(null, res)
+        })
+    },
+
+    listDatabases: function(accountName, cb) {
+      request
+        .get(API_URL + "partners/" + config.username + "/accounts/" + accountName + "/databases")
+        .auth(config.username, config.password)
         .end(function(res) {
           if (res.error) {
             return cb(res.error, res)
@@ -19,7 +43,7 @@ module.exports = function(opts) {
     createAccount: function(opts, cb) {
       request.post(API_URL + "partners/" + opts.username + "/accounts")
         .send({name: opts.accountName, adminUser: {email: opts.adminEmail, username: opts.adminUser}})
-        .auth(opts.username, opts.password)
+        .auth(config.username, config.password)
         .end(function(err, res) {
           if (res.error) {
             return cb(res.error, res)
--- main.js
+++ main.js
@@ -21,6 +21,38 @@ function viewAccounts(opts) {
       console.log("Error viewing accounts: ", res.body.message)
       return
     }
+    console.log(res.body)
+  })
+}
+
+function viewPartner(opts) {
+  var m = mongolab({username: opts.username, password: opts.password})
+  m.viewPartner(function(err, res) {
+    if (err) {
+      console.log("Error viewing partner: ", res.body.message)
+      return
+    }
+    console.log(res.body)
+  })
+}
+
+function listDatabases(opts) {
+  var m = mongolab({username: opts.username, password: opts.password})
+  m.listDatabases(opts.accountName, function(err, res) {
+    if (err) {
+      console.log("Error listing databases: ", res.body.message)
+      return
+    }
+  })
+}
+
+function createDatabase(opts) {
+  var m = mongolab({username: opts.username, password: opts.password})
+  m.createDatabase(opts, function(err, res) {
+    if (err) {
+      console.log("Error creating database: ", res.body.message)
+      return
+    }
   })
 }
 
@@ -48,5 +80,32 @@ nomnom.command('createAccount')
   .callback(createAccount)
 
 nomnom.command('viewAccounts').callback(viewAccounts)
+
+nomnom.command('viewPartner').callback(viewPartner)
+
+nomnom.command('listDatabases')
+  .options({
+    accountName: {
+      required: true
+    }
+  })
+  .callback(listDatabases)
+
+nomnom.command('createDatabase')
+  .options({
+    accountName: {
+      required: true
+    },
+    databaseName: {
+      required: true
+    },
+    plan: {
+      default: "sandbox"
+    },
+    cloud: {
+      default: 'JYC_us-sw-1'
+    }
+  })
+  .callback(listDatabases)
 
 nomnom.parse()
